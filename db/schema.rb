fc9a60ce53fa19e8badf82d30a5942239dc0eab8
Lukeveille <luke@lukeleveille.com>
2018-11-23 11:18:16 -0500

    changed deployment db info to use postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20181006133532) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "pictures", force: :cascade do |t|
     t.string   "artist"
     t.string   "title"
